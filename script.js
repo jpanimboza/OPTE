4da5d0980507eae3fd8fee3ea527b72bd1970835
jpanimboza <61572277+jpanimboza@users.noreply.github.com>
2024-02-17 16:04:06 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 var mapa = L.map('mapa').setView([0.18,-72.90],6);
- L.tileLayer('https://tiles.stadiamaps.com/tiles/stamen_watercolor/{z}/{x}/{y}.{ext}', {
-	minZoom: 1,
-	maxZoom: 16,
-	attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://www.stamen.com/" target="_blank">Stamen Design</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-	ext: 'jpg'
-}).addTo(mapa);
+L.tileLayer('http://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}.png', {
+                attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
+                subdomains: 'abcd',
+                maxZoom: 19
+ }).addTo(map);
 $.getJSON("https://jpanimboza.github.io/OPTE/tesis_2012.geojson",
 function(data){
 var clusteredPoints = L.markerClusterGroup();
